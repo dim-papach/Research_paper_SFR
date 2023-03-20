b3de7a2a0549fbd3bc77665a4536658ec0091376
dim-papach <dipapachristopoulos@gmail.com>
2023-03-20 20:28:10 +0200

    color index
    it creates a problem that will be solved in an other branch

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
-def simple_regplot(
+
+def simple_regplot(data ,
     X, Y, n_std=2, n_pts=100, ax=None, scatter_kws=None, line_kws=None, ci_kws=None):
+    df=data
     x=df[X]
     y=df[Y]
     """ Draw a regression line with error interval. """
@@ -42,10 +45,25 @@ def simple_regplot(
     ax.grid()
     ax.set_xlabel(X)
     ax.set_ylabel(Y)
-    ax.legend(loc= "best")
-    plt.savefig("graphs/"+X+"-"+Y)
-    plt.show()
+    ax.legend(loc = "best")
+    if scatter_kws == {}:
+        plt.savefig("graphs/"+X+"-"+Y)
+        plt.show()
+    else:
+        col = scatter_kws["c"].name
+        PCM=ax.get_children()[2] #get the mappable, the 1st and the 2nd are the x and y axes
+        plt.colorbar(PCM, ax=ax).set_label(col)
+        plt.savefig("graphs/"+X+"-"+Y+"color_"+col)
+        plt.show()
     return fit_results
+
+# Calculate the bin width using the Freedman-Diaconis rule
+def fd_bins(x):
+    iqr = df[x].quantile(0.75) - df[x].quantile(0.25)
+    bin_width = 2 * iqr / df[x].count()**(1/3)
+
+    # Calculate the number of bins using the bin width
+    return int((df[x].max() - df[x].min()) / bin_width)
 
 df=pd.read_csv("Karachentsev_list.csv")
 
@@ -61,34 +79,70 @@ df['K']=10**df['logKLum']
 
 df['MHI']=10**df['logMHI']
 
+df["color"]=df['Bmag']-df['Kmag']
+
 df['SFR_0']=df[ ['SFR_Ha','SFR_FUV']].mean(axis=1, skipna=True)
 df['log_SFR_0']= np.log10(df['SFR_0'])
 
 df=df[(df.SFR_0>=10**(-3))]
 
-df['Mass']=0.6*df['K']
-df["logMass"]=np.log10(df['Mass'])
+df['StellarMass']=0.6*df['K']
+df["logStellarMass"]=np.log10(df['StellarMass'])
 
-df['av_SFR']=df['Mass']*1.3/(12.5*10**9)
+df['av_SFR']=df['StellarMass']*1.3/(12.5*10**9)
+df['log_av_SFR']=np.log10(df['av_SFR'])
+
 df['ratio']=df['av_SFR']/df['SFR_0']
 df['log_ratio']=np.log10(df['ratio'])
+
+data["log_ratio"]=df["log_ratio"]
 
 df["Mg"]=1.33*df["MHI"]
 df["logMg"]=np.log10(df["Mg"])
 
-df["Mt"]=df["Mg"]+df["Mass"]
+df["Mt"]=df["Mg"]+df["StellarMass"]
 df['logMt']=np.log10(df['Mt'])
 
-print(df[ ["SFR_0","av_SFR","ratio","log_ratio",'Mt','MHI','Mass','Mg']].describe(include="all"))
+df["Mass_ratio"]=df["StellarMass"]/df["Mg"]
+df["log_Mass_ratio"]=np.log10(df["Mass_ratio"])
 
-df.plot(kind='scatter',y='av_SFR', x='SFR_0')
+print(df[ ["SFR_0","av_SFR","ratio","log_ratio",'Mt','MHI','StellarMass','Mg']].describe(include="all"))
+
+df.plot(kind='scatter',y='av_SFR', x='SFR_0', grid="True")
 plt.xscale('log')
 plt.yscale('log')
 plt.savefig('graphs/av_SFR-SFR_0')
-df.to_csv("out", sep="\t", columns=['SFR_0','av_SFR','Mass','ratio'])
+plt.show()
+simple_regplot('log_SFR_0','log_av_SFR')
+
+da=df["log_ratio"]
+print(da.describe())
+# Calculate the mean and standard deviation of the da
+mean = da.mean()
+std = da.std()
+
+# Define the lower and upper bounds for the 2-sigma range
+lower = mean - 2*std
+upper = mean + 2*std
+
+# Filter out the values outside of the 2-sigma range
+df = df[(da >= lower) & (da <= upper)]
+print(da.describe())
+iqr = data['log_ratio'].quantile(0.75) - data['log_ratio'].quantile(0.25)
+bin_width = 2 * iqr / data['log_ratio'].count()**(1/3)
+
+# Calculate the number of bins using the bin width
+binss= int((data['log_ratio'].max() - data['log_ratio'].min()) / bin_width)
+
+binsss= int((da.max() - da.min()) / bin_width)
+
+data["log_ratio"].hist(bins=binss,edgecolor= "blue")
+da.hist(bins=binsss,alpha=0.5,edgecolor='red')
 plt.show()
 
 
+df = df[(df["log_ratio"] >= lower) & (df["log_ratio"] <= upper)]
+print(df.count() , df['ratio'].describe())
 
 for i in df.index:
     def sfrx(z):
@@ -104,84 +158,152 @@ for i in df.index:
     z = fsolve(sfrx,3.0)
     df.at[i,'x']=(z)
 
-tsf=np.random.normal(12.5*10**9,1,1)
-df['tau']=tsf/df['x']
+df.hist(column='x', bins=fd_bins('x'))
 
-df['A_del']=df['SFR_0']*df['tau']*np.exp(df['x'])/df['x']
-
-print(df[ ["x", 'tau', 'A_del','Mass']].describe(include='all' ), "\n")
-
-df.plot(kind='scatter', x='x', y='A_del',c= "logMass")
-plt.xscale('log')
-plt.yscale('log')
-plt.savefig("graphs/x-A_3")
-
-df.plot(kind='scatter', x='tau', y="A_del", c= "logMass")
-plt.xscale('log')
-plt.yscale('log')
-plt.savefig("graphs/T-A_3")
-
-df.plot(kind='scatter', x='tau', y='A_del', c= "TType")
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
-
-#we can choose the number of bins acording to the Square-root choice (https://en.wikipedia.org/wiki/Histogram#Number_of_bins_and_width)
-k=int(np.ceil(np.sqrt(df.shape[0])))
-
-df.hist(column='x', bins=k)
-
-plt.xlabel('x3')
+plt.xlabel('x')
 plt.ylabel('# of event')
-
 
 plt.savefig('graphs/histogram_x3.png')
 
 plt.show()
 
+tsf=12.5*10**9
+
+df['tau']=tsf/df['x']
+df["log_tau"]=np.log10(df["tau"])
+
+df['A']=df['SFR_0']*df['tau']*np.exp(df['x'])/df['x']
+
+df["a"]=df["av_SFR"]*tsf/(1-(1+df["x"])*np.exp(-df['x']))
+
+df["A_del"]=df[ ["a","A"]].mean(axis=1, skipna=True)
+
+print(df[ ["x", 'tau','A', 'A_del','StellarMass','a']].describe(include='all' ), "\n")
+
+print(df[ ["x", 'tau','A_del']].describe(include='all' ), "\n")
+
+df.plot(kind='scatter', x='x', y='A_del',c= "logMt")
+plt.xscale('log')
+plt.yscale('log')
+plt.savefig("graphs/x-A_3")
+
+df.plot(kind='scatter', x='tau', y="A_del", c= "logMt")
+plt.xscale('log')
+plt.yscale('log')
+plt.savefig("graphs/T-A_3")
+plt.show()
+
+dp=df[["Name","TType","SFR_0","logMt","StellarMass","A_del","tau","x","color","Mass_ratio","log_Mass_ratio"]].copy()
+dp['log_tau'] = np.log10(dp.tau)
+tau=3.5*10**9
+dp["z"]=1.3*dp["StellarMass"]/tau
+
+
 for i in df.index:
     def sfrx(z):
         x = z
 
-        ratio=df.loc[i]['ratio']
+        cons=dp.loc[i]['z']
+        SFR=dp.loc[i]['SFR_0']
+
 
         #f=ratio-(np.exp(x)-np.abs(x)-1)/x**2
-        f=ratio-(np.exp(x)-np.exp(np.log(x))-1)/x**2
+        f=cons/SFR-(np.exp(x)-np.exp(np.log(x))-1)/x
         return f
 
     #for i in df.index:
     z = fsolve(sfrx,3.0)
-    df.at[i,'x_i']=(z)
+    dp.at[i,'x_i']=(z)
+dp["tsf"]=dp['x_i']*tau
+dp["log_tsf"]=np.log10(dp.tsf)
+dp["av_SFR"]=dp.z/dp.x_i
+dp['ratio']=dp.av_SFR/dp.SFR_0
+dp['A']=tau*dp['SFR_0']*np.exp(dp.x_i)/dp.x_i
 
-tau=np.random.normal(4*10**9,1,1)
-df['tsf']=tau*df['x_i']
+print(dp[['A','tsf','x_i']].describe())
 
-df['A_del_i']=df['SFR_0']*tau*np.exp(df['x'])/df['x']
-
-print(df[ ["x_i", 'tsf', 'A_del_i','A_del','Mass']].describe(include='all' ), "\n")
-
-df.plot(kind='scatter', x='x_i', y='A_del_i',c= "logMass")
+dp.plot(kind='scatter', x='x_i', y='A',c='logMt')
 plt.xscale('log')
 plt.yscale('log')
-plt.savefig("graphs/x-A_i")
+plt.savefig("graphs/x-A_tau")
+plt.show()
 
-df.plot(kind='scatter', x='tsf', y="A_del_i", c= "logMass")
-plt.xscale('log')
-plt.yscale('log')
-plt.savefig("graphs/T-A_i")
-
-df.plot(kind='scatter', x='tsf', y='A_del_i', c= "TType")
+dp['af']=dp[["A_del","A"]].mean(axis=1, skipna=True)
+dp.plot(kind='scatter', x='x_i', y='af',c='logMt')
 plt.xscale('log')
 plt.yscale('log')
 plt.show()
 
-df['TType'].value_counts(sort=False).plot(kind='bar')
+dp['log_A_del']=np.log10(dp.A_del)
+dp['log_A']=np.log10(dp.A)
+print(dp[['A','A_del','af']].describe())
+
+print(dp[['x','x_i']].describe())
+
+simple_regplot(dp,'log_A_del','log_A',scatter_kws={"c":dp["logMt"]})
+
+dp.plot(kind='scatter', x='log_tau', y='tsf',c='logMt')
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
+
+x=simple_regplot(dp,'x','x_i',scatter_kws={"c":dp["logMt"]})
+x=simple_regplot(dp,'x','x_i',scatter_kws={"c":dp["TType"]})
+x=simple_regplot(dp,'x','x_i',scatter_kws={"c":dp["color"]})
+
+simple_regplot(dp,'logMt','x_i',scatter_kws={"c":dp["color"]})
+
+print(x.summary())
+
+temp_dataf=dp[['Name','tsf']]
+df = pd.merge(df, temp_dataf , on = 'Name', how = 'outer')
+print(df['tsf'])
+
+df["tau_g"]=df["Mg"]/df["SFR_0"]
+df["log_tau_g"]=np.log10(df["tau_g"])
+print(df["tau_g"].describe())
+
+df.plot(kind="scatter",x="Mg",y="tau_g", c = 'logStellarMass')
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
+
+df.plot(kind="scatter",x="x",y="tau_g", c = 'logStellarMass')
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
+
+df.plot(kind="scatter",x="tau",y="tau_g", c = 'logStellarMass')
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
+
+df.plot(kind="scatter",x="tsf",y="tau_g", c = 'logStellarMass')
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
+
+typ=pd.read_csv("Karachentsev_list_flags.csv")
+
+typ["TType"]=typ["TType"].astype('category')
+typ["Tdw1"]=typ["Tdw1"].astype('category')
+typ["Tdw2"]=typ["Tdw2"].astype('category')
+print(typ.count())
+typ['TType'].value_counts(sort=False).plot(kind='bar',logy=True,grid = 'True')
+plt.xlabel("Morphology")
+plt.ylabel("Number of Galaxies")
 plt.savefig("graphs/hist-Type")
 plt.show()
-df['Tdw1'].value_counts(sort=False).plot(kind='bar', logy=True)
+
+typ['Tdw1'].value_counts(sort=False).plot(kind='bar', logy=True,grid = 'True')
+plt.xlabel("Dwarf galaxy morphology")
+plt.ylabel("Number of Galaxies")
 plt.savefig("graphs/hist-Tdw1")
 plt.show()
-df['Tdw2'].value_counts(sort=False).plot(kind='bar')
+
+typ['Tdw2'].value_counts(sort=False).plot(kind='bar', logy=True,grid = 'True')
+plt.xlabel("Dwarf galaxy surface brightness morphology")
+plt.ylabel("Number of Galaxies")
 plt.savefig("graphs/hist-Tdw2")
 plt.show()
 
@@ -198,40 +320,52 @@ plt.savefig('graphs/histogram_x3.png')
 
 plt.show()
 
-mhi_mass=simple_regplot("logMass","logMHI")
+mhi_mass=simple_regplot(df,"logStellarMass","logMHI",scatter_kws={"c": df["log_tau_g"]})
 print(mhi_mass.summary())
 
-mg_mass=simple_regplot("logMg","logMass")
+simple_regplot(df,"logStellarMass","logMg",scatter_kws={"c": df["color"]})
+
+mg_mass=simple_regplot(df,"logMg","logStellarMass")
 print(mg_mass.summary())
 
-mass_m26=simple_regplot("logMass","logM26")
+mass_m26=simple_regplot(df,"logStellarMass","logM26",scatter_kws={"c": df["log_tau_g"]})
 print(mass_m26.summary())
 
-mg_m26=simple_regplot("logMg","logM26")
+mg_m26=simple_regplot(df,"logMg","logM26")
 print(mg_m26.summary())
 
-mhi_m26=simple_regplot("logMHI","logM26")
+mhi_m26=simple_regplot(df,"logMHI","logM26")
 print(mhi_m26.summary())
 
-mass_mt=simple_regplot("logMass","logMt")
+mass_mt=simple_regplot(df,"logStellarMass","logMt",scatter_kws={"c": df["log_tau_g"]})
+mass_mt=simple_regplot(df,"logStellarMass","logMt",scatter_kws={"c": df["log_SFR_0"]})
+mass_mt=simple_regplot(df,"logStellarMass","logMt",scatter_kws={"c": df["logMg"]})
+mass_mt=simple_regplot(df,"logStellarMass","logMt",scatter_kws={"c": df["log_Mass_ratio"]})
+mass_mt=simple_regplot(df,"logStellarMass","logMt",scatter_kws={"c": df["log_Mass_ratio"]})
+mass_mt=simple_regplot(df,"logStellarMass","logMt",scatter_kws={"c": df["color"]})
+
 print(mass_mt.summary())
 
-mg_mt=simple_regplot("logMg","logMt")
+mg_mt=simple_regplot(df,"logMg","logMt",scatter_kws={"c":df['log_SFR_0']})
+mg_mt=simple_regplot(df,"logMg","logMt",scatter_kws={"c":df['log_tau_g']})
+mg_mt=simple_regplot(df,"logMg","logMt",scatter_kws={"c":df['logStellarMass']})
 print(mg_mt.summary())
 
-mhi_mt=simple_regplot("logMHI","logMt")
+mhi_mt=simple_regplot(df,"logMHI","logMt")
 print(mhi_mt.summary())
 
-m26_mt=simple_regplot("logM26","logMt")
+m26_mt=simple_regplot(df,"logM26","logMt")
 print(m26_mt.summary())
 
-plt.scatter(10**df["logM26"], df["Mass"],label= "M26")
-plt.scatter(df["MHI"], df["Mass"], label= "MHI")
-plt.scatter(df["Mt"], df["Mass"], label= "MHI")
+plt.scatter(10**df["logM26"], df["StellarMass"],label= "M26")
+plt.scatter(df["MHI"], df["StellarMass"], label= "MHI")
+plt.scatter(df["Mt"], df["StellarMass"], label= "MHI")
 plt.legend(loc= "upper left")
-plt.xlabel("Mass")
+plt.xlabel("StellarMass")
 plt.xscale("log")
 plt.yscale("log")
-plt.title("M26, MHI=f(Mass), log")
+plt.title("M26, MHI=f(StellarMass), log")
 plt.savefig('graphs/M-MHI-M26')
 plt.show()
+
+simple_regplot(dp,"log_tsf","log_Mass_ratio",scatter_kws={"c": df["color"]})
