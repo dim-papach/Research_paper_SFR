22e74cab5e17931e607bd55214918fa4ef1df87d
dim-papach <dipapachristopoulos@gmail.com>
2023-07-23 16:39:50 +0300

    I recompiled (i think thats all)

--- final.py
+++ final.py
@@ -7,6 +7,7 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from tabulate import tabulate
 import uncertainties as unc
+from astropy import units as u
 
 
 
@@ -19,9 +20,9 @@ def caption(fname,caption,name = None):
 
 def simple_regplot(data ,
     X, Y, cap = None, name = None, n_std=2, n_pts=100, ax=None, scatter_kws=None, line_kws=None, ci_kws=None):
-    df=data
-    x=df[X]
-    y=df[Y]
+    df_data=data
+    x=df_data[X]
+    y=df_data[Y]
     """ Draw a regression line with error interval. """
     ax = plt.gca() if ax is None else ax
 
@@ -55,6 +56,12 @@ def simple_regplot(data ,
     ax.grid()
     ax.set_xlabel(X)
     ax.set_ylabel(Y)
+    ############################
+    # if log == 1:             #
+    #     ax.set_yscale("log") #
+    #     ax.set_xscale("log") #
+    ############################
+
     ax.legend(loc = "best")
     if scatter_kws == {}:
         lab = X+"-"+Y
@@ -73,13 +80,13 @@ def simple_regplot(data ,
     if name == None:
         name = cap
 
-    Cap = "#+name: fig:{} \n#+label: fig:{} \n#+caption:{}\n#+ATTR_LaTeX: :placement [!htpb] \n[[./{}.png]]".format(cap,name,name,fname)
+    Cap = "#+name: fig:{} \n#+caption:{}\n#+ATTR_LaTeX: :placement [!htpb] \n[[./{}.png]]".format(name,cap,fname)
     return fit_results, Cap , lab
 
 # Calculate the bin width using the Freedman-Diaconis rule
 def fd_bins(x):
-    iqr = df[x].quantile(0.75) - df[x].quantile(0.25)
-    bin_width = 2 * iqr / df[x].count()**(1/3)
+    iqr = x.quantile(0.75) - x.quantile(0.25)
+    bin_width = 2 * iqr / x.count()**(1/3)
 
     # Calculate the number of bins using the bin width
     return int((df[x].max() - df[x].min()) / bin_width)
@@ -95,7 +102,7 @@ def inline_ols(y,x,ols):
         return "${} = ({:.ueSL})\cdot {} + ({:.ueSL})$".format(y, unc.ufloat(ols[0].params[1], ols[0].bse[1]), x, unc.ufloat(ols[0].params[0], ols[0].bse[0]))
 
 def inline_r2(ols):
-        return r"$R^2 = {}\%$".format(round(ols[0].rsquared*100))
+        return r"$R^2 = {}\%$ \noindent".format(round(ols[0].rsquared*100))
 
 def eq_ols(y,x,ols):
     return (r"\begin{"+"equation"+"}\label{eq:"+"{}".format(ols[2])+"}\n"+
@@ -103,7 +110,7 @@ def eq_ols(y,x,ols):
             "& {} = ({:.ueSL})\cdot {} + ({:.ueSL}) \\\ \n".format(y, unc.ufloat(ols[0].params[1], ols[0].bse[1]), x, unc.ufloat(ols[0].params[0], ols[0].bse[0]))+
             r"& \textrm"+"{"+"with correlation "+"}"+" R^2={}\%\n".format(round(ols[0].rsquared*100))+
             r"\end{"+"align"+"}\n"+
-            "\end{"+"equation"+"}\n")
+            "\end{"+"equation"+"}\n"+r"\noindent")
 
 flag=pd.read_csv("Karachentsev_list_flags.csv")
 
@@ -122,9 +129,12 @@ df['K']=10**df['logKLum']
 
 df['MHI']=10**df['logMHI']
 
-df["color"] = df["Bmag"] - df['FUVmag']
+df["color"] = -df["Bmag"] + df['FUVmag']
 
-no_col_str(df,'log').count().to_latex(position = "hc")
+temp_table = no_col_str(df,'log')
+temp_table = temp_table.count()
+temp_table.columns = ["sepal_length", "sepal_width"]
+temp_table.to_markdown(headers = ["Measurment","Number of Galaxies"] ,tablefmt= "orgtbl")
 
 ##Average SFR
 df['SFR_0']=df[ ['SFR_Ha','SFR_FUV']].mean(axis=1, skipna=True)
@@ -179,31 +189,35 @@ dts['log_av_SFR']=np.log10(dts['av_SFR'])
 dts['ratio']=dts['av_SFR']/dts['SFR_0']
 dts['log_ratio']=np.log10(dts['ratio'])
 
+def sfrx(z):
+    x =max(0,z)
+
+    ratio=dts.loc[i]['ratio']
+
+    #f=ratio-(np.exp(x)-np.abs(x)-1)/x**2
+    f=ratio-(np.exp(x)-x-1)/x**2
+    return f
+
+
 for i in dts.index:
-    def sfrx(z):
-        x = z
-
-        ratio=dts.loc[i]['ratio']
-
-        #f=ratio-(np.exp(x)-np.abs(x)-1)/x**2
-        f=ratio-(np.exp(x)-np.exp(np.log(x))-1)/x**2
-        return f
-
     #for i in dts.index:
     z = fsolve(sfrx,3.0)
     dts.at[i,'x_tsf']=(z)
+
+dts["log_x_tsf"]=np.log10(dts["x_tsf"])
 
 dts['tau']=tsf/dts['x_tsf']
 dts["log_tau"]=np.log10(dts["tau"])
 
 dts["A_tsf"]=dts["av_SFR"]*tsf/(1-(1+dts["x_tsf"])*np.exp(-dts['x_tsf']))
-
-dts[["A_tsf","tau","x_tsf"]].describe(include='all').to_latex(position = "hc")
+dts["log_A_tsf"] = np.log10(dts["A_tsf"])
+dts[["A_tsf","tau","x_tsf"]].describe(include='all').round(3).to_markdown(tablefmt= "orgtbl")
 
 fname = "figs/x-A_tsf"
 dts.plot(kind='scatter', x='x_tsf', y='A_tsf',c= "logMt")
 plt.xscale('log')
 plt.yscale('log')
+plt.grid()
 plt.savefig(fname)
 plt.close()
 caption(fname,"$A_{del} = f(x)$ for constant t_{sf}")
@@ -214,7 +228,12 @@ plt.xscale('log')
 plt.yscale('log')
 plt.savefig(fname)
 plt.close()
-fname+'.png'
+caption(fname,r"$A_{del} = f(\tau)$ for constant t_{sf}")
+
+A_tsf_Mt = simple_regplot(dts,"logMt","log_A_tsf",scatter_kws={"c": dts["x_tsf"]}, cap = r"Total Mass $M_t$ - $A_{del}|_{t_{sf}}$" ,name = "A_tsf_Mt")
+A_tsf_Mt[1]
+
+eq_ols("$log(A_{del}|_t_{sf})","$log(M_t)$",  A_tsf_Mt)
 
 ###Constant tau
 dtau=df.copy()
@@ -223,50 +242,58 @@ zeta=1.3
 
 dtau["z"]=zeta*dtau["StellarMass"]/tau
 
-for i in df.index:
-    def sfrx(z):
-        x = z
+def sfrx(var):
+    x = max(13e-9,var)
 
-        cons=dtau.loc[i]['z']
-        SFR=dtau.loc[i]['SFR_0']
+    cons=dtau.loc[i]["z"]
+    SFR=dtau.loc[i]['SFR_0']
 
 
-        #f=ratio-(np.exp(x)-np.abs(x)-1)/x**2
-        f=cons/SFR-(np.exp(x)-np.exp(np.log(x))-1)/x
-        return f
+    #f=ratio-(np.exp(x)-np.abs(x)-1)/x**2
+    f=cons/SFR-(np.exp(x)-x-1)/x
+    return f
+
+for i in dtau.index:
 
     #for i in df.index:
-    z = fsolve(sfrx,3.0)
-    dtau.at[i,'x_tau']=(z)
+    solution = fsolve(sfrx,3.0)
+    dtau.at[i,'x_tau']=solution
+dtau["log_x_tau"]=np.log10(dtau.x_tau)
 
 dtau["tsf"]=dtau['x_tau']*tau
 dtau["log_tsf"]=np.log10(dtau.tsf)
 dtau["av_SFR"]=dtau.z/dtau.x_tau
 dtau['ratio']=dtau.av_SFR/dtau.SFR_0
 dtau['A_tau']=tau*dtau['SFR_0']*np.exp(dtau.x_tau)/dtau.x_tau
-dtau=dtau.drop(["z"],axis=1)
+dtau["log_A_tau"]=np.log10(dtau.A_tau)
 
-dtau[["A_tau","x_tau","tsf"]].describe(include='all').to_latex(position = "hc")
+dtau[["A_tau","x_tau","tsf"]].describe(include='all').to_markdown(tablefmt = "orgtbl")
 
 fname = "figs/x-A_tau"
 dtau.plot(kind='scatter', x='x_tau', y='A_tau',c= "logMt")
 plt.xscale('log')
 plt.yscale('log')
+plt.grid()
 plt.savefig(fname)
 plt.close()
 
 caption(fname,r"$A_{del} = f(x)$ for constant $\tau$")
 
-fname = "figs/T-A_tau.png"
+fname = "figs/T-A_tau"
 dtau.plot(kind='scatter', x='tsf', y='A_tau',c= "logMt")
 plt.xscale('log')
 plt.yscale('log')
 plt.savefig(fname)
 plt.close()
 
-fname
+caption(fname,r"$A_{del} = f(t_{sf})$ for constant $\tau$")
 
-dp=pd.merge(dtau[["Name","A_tau", "x_tau", "tsf"]], dts, on = 'Name')
+A_tau_Mt = simple_regplot(dtau,"logStellarMass","log_A_tau",scatter_kws={"c": dtau["x_tau"]}, cap = r"Total Mass $M_t$ - $A_{del}|_{\tau}$", name = "A_tau_Mt")
+A_tau_Mt[1]
+
+eq_ols(r"$log(A_{del}|_\tau)", "$log(M_t)$", A_tau_Mt)
+
+dp=pd.merge(dtau[["Name","A_tau","log_A_tau", "x_tau", "tsf"]], dts, on = 'Name')
 dp["log_x_tau"]=np.log10(dp["x_tau"])
 dp["log_x_tsf"]=np.log10(dp["x_tsf"])
 dp["log_tau"]=np.log10(dp["tau"])
@@ -345,6 +372,192 @@ plt.yscale('log')
 plt.savefig(fname)
 plt.close()
 caption(fname, "Comparison of the 2 A_{del}s according to their total masses")
+
+dcl = dp.copy()
+
+fname = "figs/A_tau-A_tsf_Mt"
+dp.plot.scatter(x = "A_tsf",
+                y = "A_tau",
+                c = "logMt", grid = True)
+plt.xscale('log')
+plt.yscale('log')
+plt.savefig(fname)
+plt.close()
+caption(fname, "Comparison of the 2 A_{del}s according to their total masses")
+
+dcl['const_tsf']=np.exp(dcl.x_tsf)/(np.exp(dcl.x_tsf)-dcl.x_tsf-1)
+dcl['const_tau']=np.exp(dcl.x_tau)/(np.exp(dcl.x_tau)-dcl.x_tsf-1)
+dcl['A_theor_tsf']=dcl['StellarMass']*dcl.const_tsf
+dcl['A_theor_tau']=dcl['StellarMass']*dcl.const_tau
+dcl["log_A_theor_tsf"]=np.log10(dcl["A_theor_tsf"])
+dcl["log_A_theor_tau"]=np.log10(dcl["A_theor_tau"])
+
+fname = "figs/A_theor_tau-M*"
+dcl.plot.scatter(x = "A_theor_tsf",
+                y = "A_theor_tau",
+                c = "x_tau",
+                grid = True)
+plt.xscale('log')
+plt.yscale('log')
+plt.savefig(fname)
+plt.close()
+caption(fname, "Comparison of the 2 A_{del}s according to their total masses")
+
+A_theor_tau_StellarMass_log=simple_regplot(dcl,"log_A_theor_tau","logStellarMass",scatter_kws={"c":dcl["x_tsf"]}, cap = r"Comparison of the A_del according to their Stellar Mass")
+A_theor_tau_StellarMass_log[1]
+
+A_theor_A_exp_tau=simple_regplot(dcl,"log_A_theor_tau","log_A_tau",scatter_kws={"c":dcl["x_tsf"]}, cap = r"Comparison of the 2 $A_{del}|_{\tau=const.}$s (theoretical and experimental)", name = "A_theor_A_exp_tau")
+A_theor_A_exp_tau[1]
+
+A_theor_A_exp_tsf=simple_regplot(dcl,"log_A_theor_tsf","log_A_tsf",scatter_kws={"c":dcl["x_tsf"]}, cap = r"Comparison of the 2 $A_{del}|_{tsf=const.}$s (theoretical and experimental)", name = "A_theor_A_exp_tsf")
+A_theor_A_exp_tsf[1]
+
+t = np.linspace(0.1, 5, 900)
+fname = "figs/SFR_avSFR"
+
+plt.plot(t, t/(np.exp(t)-t-1), label = "$SFR_{0,del}$")
+plt.plot(t, 1/t, label = "$\overline{SFR_{del}}$")
+plt.legend(loc ="upper right")
+plt.grid()
+plt.xlabel("$t_{sf}$")
+plt.ylabel("$SFR$")
+plt.savefig(fname)
+plt.close()
+
+caption(fname, r"The $SFR_{0,del}$ and $\overline{SFR_{del}}$ for constant $\tau=1$ and $\zeta M_*=1$")
+
+dsol = df.copy()
+dsol.SFR_0.describe()
+
+def positive_constraint(vars):
+    x = max(1e-11, vars[0])  # Ensures x is positive or zero
+    y = max(1e-11, vars[1])  # Ensures y is positive or zero
+    return x, y
+def solved(var):
+
+    cons = positive_constraint(var)
+    tsf = cons[0]
+    tau = cons[1]
+    x = tsf/tau
+
+    SFR=dsol.loc[i]["SFR_0"]
+    Mstellar=dsol.loc[i]["StellarMass"]
+
+    A = zeta*Mstellar*np.exp(x)/(np.exp(x)-(x)-1)
+
+    f = np.zeros(2)
+
+    #####################################################
+    # f[0] = tau*SFR-A*x*np.exp(-x)                     #
+    # f[1] = tsf*(np.exp(x)-x-1)*SFR-zeta*Mstellar*x**2 #
+    #####################################################
+
+    f[0] = SFR*tau*np.exp(x)-A*x
+    f[1] = zeta*Mstellar*x**2/tsf/SFR-(np.exp(x)-x-1)
+    return f
+
+for i in dsol.index:
+
+    #for i in dts.index:
+    z = fsolve(solved,[9.6*10**9,3.5*10**9])
+    dsol.at[i,'tsf']=z[0]
+    dsol.at[i,'tau']=z[1]
+
+dsol["x"]=dsol.tsf/dsol.tau
+
+def solved2(var):
+
+    cons = positive_constraint(var)
+    tsf = cons[0]
+    tau = cons[1]
+    x = tsf/tau
+
+    SFR=dsol.loc[i]["SFR_0"]
+    Mstellar=dsol.loc[i]["StellarMass"]
+
+    A = zeta*Mstellar*np.exp(x)/(np.exp(x)-(x)-1)
+
+    f = np.zeros(2)
+
+    f[0] = tau*SFR-A*x*np.exp(-x)
+    f[1] = tsf*(np.exp(x)-x-1)*SFR-zeta*Mstellar*x**2
+
+    #####################################################
+    # f[0] = SFR*tau*np.exp(x)-A*x                      #
+    # f[1] = zeta*Mstellar*x**2/tsf/SFR-(np.exp(x)-x-1) #
+    #####################################################
+    return f
+
+for i in dsol.index:
+
+    #for i in dts.index:
+    z = fsolve(solved2,[9.6*10**9,3.5*10**9])
+    dsol.at[i,'tsf2']=z[0]
+    dsol.at[i,'tau2']=z[1]
+
+dsol["x2"]=dsol.tsf2/dsol.tau2
+
+dsol['tsfG'] = dsol.tsf/10**9
+dsol['tauG'] = dsol.tau/10**9
+
+fname = "figs/tsf-hist"
+plt.hist(dsol["tsfG"],range=[0,20], bins =20)
+plt.grid(True)
+plt.xlabel("$t_{sf}$")
+plt.ylabel("Number of Galaxies")
+plt.savefig(fname)
+plt.close()
+
+caption(fname, "Histogram of t_{sf} from 0 to 20 Gyr")
+
+dsol[["tsfG","tauG","x"]].describe(include='all').round(3).to_markdown(tablefmt= "orgtbl")
+
+dsol['tsfG2'] = dsol.tsf2/10**9
+dsol['tauG2'] = dsol.tau2/10**9
+dsol[["tsfG2","tauG2", "x2"]].describe(include='all').round(3).to_markdown(tablefmt= "orgtbl")
+
+fname = "figs/tsf2-hist"
+plt.hist(dsol["tsfG2"],range=[0,20], bins =20)
+plt.grid(True)
+plt.xlabel("$t_{sf}$")
+plt.ylabel("Number of Galaxies")
+plt.savefig(fname)
+plt.close()
+
+caption(fname, "Histogram of t_{sf} from 0 to 20 Gyr")
+
+fname = "figs/tsf_tsf2"
+m = 1
+for i in ["tsfG","tsfG2"]:
+    plt.scatter(dsol[i], dsol["color"], label = "Method {}".format(m) )
+    m=m+1
+plt.xscale("log")
+plt.xlabel(r"$t_{sf}$ Gyr")
+plt.ylabel("Color index")
+plt.legend(loc = "upper right")
+plt.grid()
+plt.savefig(fname)
+plt.close()
+
+caption(fname, r"Comparing the two $t_{sf}$")
+
+fname = "figs/tsf_tsf2"
+m = 1
+for i in ["tsfG","tau"]:
+    plt.scatter(dsol[i], dsol["color"],label = "Method {}".format(m) )
+    m=m+1
+plt.xscale("log")
+plt.xlabel(r"$t_{sf}$ Gyr")
+plt.ylabel("Color index")
+plt.legend(loc = "upper right")
+plt.grid()
+plt.savefig(fname)
+plt.close()
+
+caption(fname, r"Comparing the two $t_{sf}$")
+
+color_mass=simple_regplot(dsol,"logMt","color",scatter_kws={"c":dcl["tsf"]}, cap = r"Comparison of the 2 $A_{del}|_{tsf=const.}$s (theoretical and experimental)", name = "A_theor_A_exp_tsf")
+color_mass[1]
 
 cols_to_use = dp.columns.difference(df.columns)
 dtg = pd.merge(df, dp[cols_to_use], left_index=True, right_index=True, how='outer')
@@ -451,7 +664,7 @@ SFR_tg_SMass = simple_regplot(dm, "log_SFR_0", "log_tau_g", scatter_kws = {"c":d
 
 SFR_Mg_tg = simple_regplot(dm, "log_SFR_0", "logMg", scatter_kws = {"c":dm["log_tau_g"]})
 
-SFR_Mt_tg = simple_regplot(dm, "logMt", "log_SFR_0" scatter_kws = {"c":dm["log_tau_g"]})
+SFR_Mt_tg = simple_regplot(dm, "logMt", "log_SFR_0", scatter_kws = {"c":dm["log_tau_g"]})
 
 SFR_col = simple_regplot(dm, "log_SFR_0", "color")
 SFR_col[1]
@@ -463,3 +676,5 @@ SFR_tg_SMass[1]
 SFR_Mt_tg[1]
 
 simple_regplot(dm, "logMt", "log_tau", scatter_kws = {"c":dm["log_tau_g"]})
+
+simple_regplot(dm, "logMt", "log_tsf", scatter_kws = {"c":dm["log_tau_g"]})
