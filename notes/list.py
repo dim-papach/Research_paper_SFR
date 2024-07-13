a2324936edf1e5cd80ac314c202103287602a635
dim-papach <dipapachristopoulos@gmail.com>
2024-07-14 01:02:22 +0300

    Merge tables and delete columns with the same name and galaxies with long names

--- notes/list.py
+++ notes/list.py
@@ -1,13 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 from astropy.io import ascii
 from astropy.coordinates import SkyCoord
 from astropy.table import Table, join, QTable
-from astropy.utils.diff import report_diff_values
 import astropy.units as u
+from astropy.visualization import quantity_support
 import glob
 import os
 
- 
+# Function to clean the data before reading into a Table
+def clean_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    cleaned_lines = []
+    for line in lines:
+        if line[19:21].strip().endswith('.'):
+            line = line[:19] + line[19:21].replace('.', '') + line[21:]
+        cleaned_lines.append(line)
+    
+    cleaned_content = ''.join(cleaned_lines)
+    return cleaned_content
 
 # Change directory to where the notes are located
 os.chdir("/home/dp/Documents/Research_paper_SFR/notes")
@@ -17,7 +31,10 @@ file_pattern = "../tables/lvg_table*.dat"
 file_paths = sorted(glob.glob(file_pattern))
 
 # Create a list to store Tables
-dq = [Table(ascii.read(file_path, format="mrt"), masked=True) for file_path in file_paths]
+dq = []
+for file_path in file_paths:
+    cleaned_content = clean_file(file_path)
+    dq.append(Table(ascii.read(cleaned_content, format="mrt"), masked=True))
 
 # Table names
 nm = ["cat", "param", "magn", "vel", "kin", "dist", "sfr"]
@@ -26,13 +43,25 @@ nm = ["cat", "param", "magn", "vel", "kin", "dist", "sfr"]
 for name, table in zip(nm, dq):
     globals()[name] = table
 
-# Clean RAh column in all tables that contain it
-def clean_ra_column(table):
-    if 'RAh' in table.colnames:
-        table['RAh'] = [int(str(x).replace('.', '')) if '.' in str(x) else x for x in table['RAh']]
 
-for table in dq:
-    clean_ra_column(table)
+# Function to create scatter plot and calculate R^2
+def scatter_and_r2(table1, table2, col1_name, col2_name, title):
+    # Join tables based on 'Name'
+    temp = join(table1["Name", col1_name], table2["Name", col2_name], join_type='inner', keys="Name")
+    
+    x = temp[temp.colnames[1]]
+    y = temp[temp.colnames[2]]
+    r2 = r2_score(x, y)
+
+    # Plot the scatter plot
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, alpha=0.5)
+    plt.xlabel(col1_name)
+    plt.ylabel(col2_name)
+    plt.title(f'{title}\n$R^2$ = {r2:.2f}')
+    plt.grid(True)
+    plt.savefig(title)
+    plt.close()
 
 # Modify magnitudes table
 magn.remove_column("r_mag")
@@ -66,35 +95,28 @@ param.rename_column("a26", "A26")
 param.rename_column("AB", "AB_int")
 
 # Compare and remove columns in cat and kin tables
-identical = report_diff_values(cat["Name", "W50"], kin["Name", "W50"])
-cat.remove_column("W50")
-kin.remove_column("r_W50")
+scatter_and_r2(cat, kin, "W50", "W50", "cat vs kin W50")
+if len(cat["W50"]) > len(kin["W50"]):
+    kin.remove_column("W50")
+else:
+    cat.remove_column("W50")
 
 # Compare param and sfr tables, and adjust BMag columns
-with open('diff_report.txt', 'w') as file:
-    report_diff_values(param["Name", "BMag"], sfr["Name", "BMag"], rtol=0.7, atol=0.7, fileobj=file)
-
-bmag = join(param["Name", "BMag"], sfr["Name", "BMag"], join_type='outer', keys="Name")
-missing_indices = np.isnan(bmag['BMag_2'])
-bmag['BMag_2'][missing_indices] = bmag['BMag_1'][missing_indices]
-bmag.rename_column("BMag_2", "BMag")
-bmag.remove_column("BMag_1")
-
-# Update sfr table
-sfr.remove_column("BMag")
-param.remove_column("BMag")
-sfr = QTable(join(sfr, bmag, join_type="outer"))
+scatter_and_r2(param, sfr, "BMag", "BMag", "param vs sfr BMag")
+if len(param["BMag"]) > len(sfr["BMag"]):
+    param.remove_column("BMag")
+else:
+    sfr.remove_column("BMag")
 
 # Compare and remove RA columns in param and sfr tables
-with open('diff_report_RAh.txt', 'w') as file:
-    report_diff_values(param["Name", "RAh"], sfr["Name", "RAh"], fileobj=file)
+scatter_and_r2(param, sfr, "RAh", "RAh", "param vs sfr RAh")
 
 columns_to_remove = ["RAh", "RAm", "RAs", "DE-", "DEd", "DEm", "DEs"]
 for col in columns_to_remove:
     sfr.remove_column(col)
 
 # Compare cat and sfr tables, and adjust TType columns
-report_diff_values(cat["Name", "TType"], sfr["Name", "T"])
+scatter_and_r2(cat, sfr, "TType", "T", "cat vs sfr TType")
 ttype = Table(join(cat["Name", "TType"], sfr["Name", "T"], join_type='outer', keys="Name"))
 sfr.remove_column("T")
 
