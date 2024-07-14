0d52de609bc5f8d2417690b3f494881f93e44636
dim-papach <dipapachristopoulos@gmail.com>
2024-07-15 02:12:40 +0300

    stilts D<11+joins

---
+++ tables/hecate_lcv_join.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Keep only the LCV from Hecate
+stilts tpipe in=HECATE_v1.1.fits  cmd="select D<11" out=HECATE_LCV.fits
+
+#Outter join for each row of hecate
+stilts tmatch2 in1=final_table.fits in2=HECATE_LCV.fits out=inner_join.fits\
+    join=all2 matcher=sky3d\
+    values1="Coordinates.ra Coordinates.dec Dis" values2="RA DEC D" params=0.5 
+#Inner join
+stilts tmatch2 in1=final_table.fits in2=HECATE_LCV.fits out=inner_join.fits\
+    join=1and2 matcher=sky3d\
+    values1="Coordinates.ra Coordinates.dec Dis" values2="RA DEC D" params=0.5 
