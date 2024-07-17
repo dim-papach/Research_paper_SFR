ff1e7542eca9fc0b694ec5b8b3942089ff1ae61f
dim-papach <dipapachristopoulos@gmail.com>
2024-07-17 21:18:16 +0300

    changed distance error to 0.2 Mpc

--- tables/hecate_lcv_join.sh
+++ tables/hecate_lcv_join.sh
@@ -7,8 +7,8 @@ stilts tpipe in=HECATE_v1.1.fits  cmd="select D<11" out=HECATE_LCV.fits
 #Outter join for each row of hecate
 stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=inner_join.fits\
     join=all2 matcher=sky3d\
-    values1="Ra Dec Dis" values2="RA DEC D" params=0.5 
+    values1="Ra Dec Dis" values2="RA DEC D" params=0.2 
 #Inner join
 stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=inner_join.fits\
     join=1and2 matcher=sky3d\
-    values1="Ra Dec Dis" values2="RA DEC D" params=0.5 
+    values1="Ra Dec Dis" values2="RA DEC D" params=0.2 
