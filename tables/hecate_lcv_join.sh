a7f02a99c2dd3af959326c16413dc5160c26138a
dim-papach <dipapachristopoulos@gmail.com>
2024-07-17 22:22:56 +0300

    fits->ecsv for simplicity

--- tables/hecate_lcv_join.sh
+++ tables/hecate_lcv_join.sh
@@ -5,10 +5,10 @@
 stilts tpipe in=HECATE_v1.1.fits  cmd="select D<11" out=HECATE_LCV.fits
 
 #Outter join for each row of hecate
-stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=inner_join.fits\
+stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=outer_join.ecsv\
     join=all2 matcher=sky3d\
     values1="Ra Dec Dis" values2="RA DEC D" params=0.2 
 #Inner join
-stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=inner_join.fits\
+stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=inner_join.ecsv\
     join=1and2 matcher=sky3d\
     values1="Ra Dec Dis" values2="RA DEC D" params=0.2 
