772e0bb5167618950e455b8ca21523a2375e3955
dim-papach <dipapachristopoulos@gmail.com>
2024-09-23 02:19:42 +0300

    new joins for unique galaxies

--- tables/hecate_lcv_join.sh
+++ tables/hecate_lcv_join.sh
@@ -2,13 +2,23 @@
 
 #Keep only the LCV from Hecate
 
-stilts tpipe in=HECATE_v1.1.fits  cmd="select D<11" out=HECATE_LCV.fits
+#stilts tpipe in=HECATE_v1.1.fits  cmd="select D<11" out=HECATE_LCV.fits
 
 #Outter join for each row of hecate
 stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=outer_join.ecsv\
-    join=all2 matcher=sky3d\
-    values1="Ra Dec Dis" values2="RA DEC D" params=0.1 
+    join=all2 matcher=sky\
+    values1="Ra Dec" values2="RA DEC" params=2 
 #Inner join
 stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=inner_join.ecsv\
-    join=1and2 matcher=sky3d\
-    values1="Ra Dec Dis" values2="RA DEC D" params=0.1 
+    join=1and2 matcher=sky\
+    values1="Ra Dec" values2="RA DEC " params=2 
+
+#Hecate not LCV
+stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=HEC_not_LCV_join.ecsv\
+    join=2not1 matcher=sky\
+    values1="Ra Dec" values2="RA DEC" params=2
+    
+#LCV not Hecate  
+stilts tmatch2 in2=final_table.ecsv in1=HECATE_LCV.fits out=LCV_not_HEC_join.ecsv\
+    join=2not1 matcher=sky\
+    values1="Ra Dec" values2="RA DEC" params=2
