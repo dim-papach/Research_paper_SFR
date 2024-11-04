2d4190b3ccef49e0aecca6df0c557680fb09085e
dim-papach <dipapachristopoulos@gmail.com>
2024-11-04 21:27:17 +0200

    fixed outer join
    - all2 -> 1or2
    - updated the catalog

--- pipeline.sh
+++ pipeline.sh
@@ -7,3 +7,5 @@ cd ../notes
 /bin/python3 list.py
 cd ../tables
 ./hecate_lcv_join.sh
+cd ../compare
+quarto render compare.qmd
--- tables/hecate_lvg_join.sh
+++ tables/hecate_lvg_join.sh
@@ -6,19 +6,23 @@
 
 #Outter join for each row of hecate
 stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=outer_join.ecsv\
-    join=all2 matcher=sky\
+    join=1or2 matcher=sky\
     values1="Ra Dec" values2="RA DEC" params=2 
+    
+echo "Done Outter"    
 #Inner join
 stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=inner_join.ecsv\
     join=1and2 matcher=sky\
     values1="Ra Dec" values2="RA DEC " params=2 
-
+    
+echo "Done Inner"    
 #Hecate not LCV
 stilts tmatch2 in1=final_table.ecsv in2=HECATE_LCV.fits out=HEC_not_LVG_join.ecsv\
     join=2not1 matcher=sky\
     values1="Ra Dec" values2="RA DEC" params=2
-    
+echo "Done Hecate not LCV"    
 #LCV not Hecate  
 stilts tmatch2 in2=final_table.ecsv in1=HECATE_LCV.fits out=LVG_not_HEC_join.ecsv\
     join=2not1 matcher=sky\
     values1="Ra Dec" values2="RA DEC" params=2
+echo "Done LCV not Hecate"    
