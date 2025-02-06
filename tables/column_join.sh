4be3f3456f8e79c387b44dea4c76ec794456a858
dim-papach <dipapachristopoulos@gmail.com>
2025-02-06 02:39:52 +0200

    fixed SFR and M total

--- tables/column_join.sh
+++ tables/column_join.sh
@@ -9,6 +9,6 @@ stilts tpipe \
   cmd='addcol -units "log(Lsol)" -desc "Merge logK" merge_logKLum "NULL_logL_K ? logKLum : logL_K"' \
   cmd='addcol -units "mag" -desc "Merge K mag" merge_Kmag "NULL_K ? Kmag : K"' \
   cmd='addcol -units "mag" -desc "Merge B mag" merge_Bmag "NULL_BT ? mag_B : BT"' \
-  cmd='addcol -units "log(Msol/yr)" -desc "Merge of log SFR" logSFR_total "NULL_logSFR_HEC ? 0.7*logSFR_UNGC : logSFR_HEC"'\
+  cmd='addcol -units "log(Msol/yr)" -desc "Merge of log SFR" logSFR_total "NULL_logSFR_HEC ? 0.7*logSFR_UNGC-0.6 : logSFR_HEC"'\
   cmd='addcol -units "Msol/yr" -desc "Total SFR in linear scale" SFR_total "exp10(logSFR_total)"'\
-  cmd='addcol -units "log(Msol)" -desc "" logSFR_total "NULL_logSFR_HEC ? 0.7*logSFR_UNGC : logSFR_HEC"'\
+  cmd='addcol -units "log(Msol)" -desc "" logM_total "NULL_logM_HEC ? log10(0.82)+merge_logKLum: logM_HEC"'\
