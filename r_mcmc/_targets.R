ff044034c197f4b83f2e0769b43e16eef4d6e70e
dim-papach <dipapachristopoulos@gmail.com>
2025-02-17 21:35:49 +0200

    tex and cleanup

--- r_mcmc/_targets.R
+++ r_mcmc/_targets.R
@@ -12,7 +12,7 @@ library(future)
 
 # Set target options:
 tar_option_set(
-  packages = c("readr", "tidyverse","tibble", "stringr", "posterior","bayesplot", "loo","ggplot2", "scales"),
+  packages = c("readr", "tidyverse", "tibble", "stringr", "posterior", "bayesplot", "loo", "ggplot2", "scales", "latex2exp", "ggeasy"),
   # format = "qs", # Optionally set the default storage format. qs is fast.
   #
   # Pipelines that take a long time to run may benefit from
@@ -24,13 +24,13 @@ tar_option_set(
   # which run as local R processes. Each worker launches when there is work
   # to do and exits if 60 seconds pass with no tasks to run.
   #
-  #controller = crew::crew_controller_local(workers = 2)
+  controller = crew::crew_controller_local(workers = 6)
   #
   # Alternatively, if you want workers to run on a high-performance computing
   # cluster, select a controller from the {crew.cluster} package.
   # For the cloud, see plugin packages like {crew.aws.batch}.
   # The following example is a controller for Sun Grid Engine (SGE).
-  # 
+  #
   #   controller = crew.cluster::crew_controller_sge(
   #     # Number of workers that the pipeline can scale up to:
   #     workers = 10,
@@ -53,9 +53,10 @@ tar_source()
 
 # Replace the target list below with your own:
 # Define the pipeline
-#list(
+# list(
 tar_plan(
-  # Step 1: Load and prepare data
+
+  #### Load and prepare data####
   tar_target(
     csv_file_path,
     "filled.csv",
@@ -65,17 +66,26 @@ tar_plan(
     read_data,
     read.csv(csv_file_path)
   ),
+  # add id_number to the data
+  tar_target(
+    id_data,
+    {
+      data <- read_data
+      data$id_number <- 1:nrow(data)
+      data
+    }
+  ),
   tar_target(
     sfr_data,
-    prepare_data(read_data)
+    prepare_data(id_data)
   ),
-  
-  # Step 2: Fit the Stan model
+
+  #### Fit the Stan model####
   tar_stan_mcmc(
     stan_fit,
     stan_files = "x.stan",
-    #stdout = "out.txt",
-    #stderr = "error.txt",
+    # stdout = "out.txt",
+    # stderr = "error.txt",
     data = list(
       N = nrow(sfr_data),
       logSFR_total = sfr_data$logSFR_total,
@@ -84,11 +94,12 @@ tar_plan(
     ),
     chains = 7,
     parallel_chains = 8,
-    iter_sampling = 4500,
-    iter_warmup = 3000,
-    init = init_function(7,1761),
-    
+    iter_sampling = 5000,
+    iter_warmup = 2500,
+    init = init_function(7, 1761),
   ),
+
+  ######## summaries#######
 
   tar_stan_summary(
     id_summary,
@@ -100,16 +111,6 @@ tar_plan(
     ),
     variables = "id"
   ),
- # tar_stan_summary(
- #    sfr_summary_t,
- #    fit = stan_fit_mcmc_x,
- #    data = list(
- #      N = nrow(sfr_data),
- #      logSFR_total = sfr_data$logSFR_total,
- #      id_numbers = sfr_data$id_number
- #    ),
- #    variables = "logSFR_total"
- #  ),
   tar_stan_summary(
     sfr_summary,
     fit = stan_fit_mcmc_x,
@@ -120,7 +121,6 @@ tar_plan(
     ),
     variables = "logSFR_today"
   ),
-
   tar_stan_summary(
     t_sf_summary,
     fit = stan_fit_mcmc_x,
@@ -129,9 +129,9 @@ tar_plan(
       logSFR_total = sfr_data$logSFR_total,
       id_numbers = sfr_data$id_number
     ),
-    variables = "t_sf"),
-  
-    tar_stan_summary(
+    variables = "t_sf"
+  ),
+  tar_stan_summary(
     log_tsf_summary,
     fit = stan_fit_mcmc_x,
     data = list(
@@ -139,8 +139,8 @@ tar_plan(
       logSFR_total = sfr_data$logSFR_total,
       id_numbers = sfr_data$id_number
     ),
-    variables = "log_tsf"),
-
+    variables = "log_tsf"
+  ),
   tar_stan_summary(
     tau_summary,
     fit = stan_fit_mcmc_x,
@@ -151,7 +151,6 @@ tar_plan(
     ),
     variables = "tau"
   ),
-
   tar_stan_summary(
     logtau_summary,
     fit = stan_fit_mcmc_x,
@@ -172,7 +171,6 @@ tar_plan(
     ),
     variables = "logA"
   ),
-  
   tar_stan_summary(
     x_summary,
     fit = stan_fit_mcmc_x,
@@ -188,80 +186,64 @@ tar_plan(
     stan_fit_diagnostics_x$divergent__
   ),
 
-  # Define the list of variables
+  ######## Define the list of variables########
   tar_target(
     summary_variables,
-    c("logSFR_today","log_tsf", "t_sf", "tau", "logA", "logtau") # List of variables
+    c("logSFR_pred", "log_tsf", "t_sf", "tau", "logA", "logtau") # List of variables
   ),
-
-  # Compute plots dynamically for mean vs median
   tar_target(
-    mean_median_plots,
+    summary_data,
     {
-      # Extract summary data for the variable
       summary_data <- switch(summary_variables,
-                             "logA" = A_summary,
-                             "tau" = tau_summary,
-                             "t_sf" = t_sf_summary,
-                             "log_tsf" = log_tsf_summary,
-                             "logSFR_today" = sfr_summary,
-                             #"logSFR_total" = sfr_summary_t,
-                             "logtau" = logtau_summary)
+        "logA" = A_summary,
+        "tau" = tau_summary,
+        "t_sf" = t_sf_summary,
+        "log_tsf" = log_tsf_summary,
+        "logSFR_pred" = sfr_summary,
+        # "logSFR_total" = sfr_summary_t,
+        "logtau" = logtau_summary
+      )
 
-      # Generate the plot
-      ggplot(summary_data, aes(x = mean, y = median)) +
-        geom_point() +
-        labs(
-          title = paste("Mean vs. Median of", summary_variables),
-          x = "Mean",
-          y = "Median"
-        )
-      ggsave(sprintf("plots/Mean-Median/%s.png", summary_variables))
+      summary_data
+    },
+    pattern = map(summary_variables) # Branch over variables
+  ),
+  tar_target(
+    summary_names, # in latex2exp format
+    {
+      switch(summary_variables,
+        "logSFR_pred" = TeX("$\\log_{10}\\left[\\frac{SFR_{pred}}{M_o/yr}\\right]$"),
+        "log_tsf" = TeX("$log_{10}\\left[\\frac{t_{sf}}{Gyr}\\right]$"),
+        "t_sf" = TeX("$t_{sf}$ [Gyr]"),
+        "tau" = TeX("$\\tau$ [Gyr]"),
+        "logA" = TeX("$\\log\\left[\\frac{A_{del}}{M_o/yr}\\right]$"),
+        "logtau" = TeX("$log_{10}\\left[\\frac{\\tau}{Gyr}\\right]$")
+      )
     },
     pattern = map(summary_variables) # Branch over variables
   ),
 
-  # Compute plots dynamically for mean vs median
+
+  ######## plots########
+  #### Compute plots dynamically for mean vs median ####
   tar_target(
-      histograms,
+    mean_median_plots,
     {
       # Extract summary data for the variable
-      summary_data <- switch(summary_variables,
-                             "logA" = A_summary,
-                             "tau" = tau_summary,
-                             "t_sf" = t_sf_summary,
-                             "log_tsf" = log_tsf_summary,
-                             "logSFR_today" = sfr_summary,
-                             "logtau" = logtau_summary)
-      max_count <- max(ggplot_build(
-        ggplot(summary_data, aes(x = mean)) +
-          geom_histogram(bins = 30)
-      )$data[[1]]$count, na.rm = TRUE)
 
-      ggplot(summary_data, aes(x = mean)) +
-        geom_histogram(alpha = 0.7, bins = 40, fill = "blue", color = "black") +
-        geom_vline(aes(xintercept = mean(mean)), color = "red", linetype = "dashed", size = 1) +
-        geom_text(
-          aes(
-            x = mean(mean),
-            label = format(mean(mean), scientific = TRUE, digits = 2) # Scientific notation, 2 decimal points
-          ),
-          y = max_count + 1, # Place the text slightly above the maximum bar
-          inherit.aes = FALSE,
-          vjust = 0, # Above the bar
-          hjust = 0.5 # Centered above the mean line
-        ) +
-        labs(
-          title = paste("Histogram of Mean for", summary_variables),
-          x = "Mean",
-          y = "Count"
-        )
+      # Generate the plot
+      mean_vs_median(summary_data, summary_variables)
+    },
+    pattern = map(summary_variables, summary_data) # Branch over variables
+  ),
 
-
-      ggsave(sprintf("plots/Hists/%s.png", summary_variables))
-     },
-
-    pattern = map(summary_variables) # Branch over variables
+  # Compute histograms dynamically
+  tar_target(
+    histograms,
+    {
+      plot_histograms(summary_data, summary_variables, summary_names)
+    },
+    pattern = map(summary_data, summary_variables, summary_names) # Branch over variables
   ),
 
 
@@ -271,36 +253,85 @@ tar_plan(
     metrics,
     c("rhat", "ess_bulk", "ess_tail")
   ),
-  
   tar_target(
     hist_metrics,
-    {
-      mean_value <- mean(stan_fit_summary_x[[metrics]], na.rm = TRUE)  # Calculate mean dynamically
-      
-      ggplot(data = stan_fit_summary_x, aes_string(x = metrics)) +
-        geom_histogram(bins = 30, fill = "blue", alpha = 0.6) +
-        geom_vline(
-          aes(xintercept = mean_value),
-          color = "red", linetype = "dashed", size = 1
-        ) +
-        annotate(
-          "text", x = mean_value, y = Inf, 
-          label = sprintf("Mean: %.2f", mean_value), 
-          vjust = 2, color = "red", size = 4
-        ) +
-        scale_x_continuous(trans='log10',
-                           breaks=trans_breaks('log10', function(x) 10^x),
-                           labels=trans_format('log10', math_format(10^.x))) +
-        labs(
-          title = sprintf("Histogram of %s", metrics),
-          x = metrics,
-          y = "Frequency"
-        )
-      
-      ggsave(sprintf("plots/%s.png", metrics))
-    },
-    pattern = map(metrics),
+    plot_metrics(summary_data, summary_variables, metrics),
+    pattern = cross(map(summary_data, summary_variables), metrics),
     format = "file"
-  )
+  ),
 
+  #### join the data####
+  tar_target(
+    all_means,
+    {
+      # Create a tibble with mean values from each summary, and their corresponding sigma values
+      summary_means <- tibble::tibble(
+        id_number = id_summary$mean, # Assuming id is the identifier
+        logSFR_pred = sfr_summary$mean,
+        logSFR_pred_sigma = sfr_summary$sd,
+        log_tsf = log_tsf_summary$mean,
+        log_tsf_sigma = log_tsf_summary$sd,
+        t_sf = t_sf_summary$mean,
+        t_sf_sigma = t_sf_summary$sd,
+        tau = tau_summary$mean,
+        tau_sigma = tau_summary$sd,
+        logtau = logtau_summary$mean,
+        logtau_sigma = logtau_summary$sd,
+        logA = A_summary$mean,
+        logA_sigma = A_summary$sd,
+        x = x_summary$mean,
+        x_sigma = x_summary$sd
+      )
+
+
+      # Return the table
+      summary_means
+    }
+  ),
+  # Join with sfr_data based on 'id'
+  tar_target(
+    joined_data,
+    {
+      # Join the data
+      joined_data <- dplyr::left_join(sfr_data, all_means, by = "id_number")
+
+      # Return the joined data
+      joined_data
+    }
+  ),
+  tar_target(
+    save_joined_data,
+    {
+      # Save the joined data
+      write.csv(joined_data, "joined_data.csv")
+    }
+  ),
+
+  #### Comparisons####
+  # target to compare sfr_pred and sfr_obs visually
+  tar_target(
+    sfr_comparison,
+    {
+      # Create a scatter plot of logSFR_pred vs logSFR_total
+      ggplot(joined_data, aes(y = logSFR_pred, x = logSFR_total)) +
+        geom_point(size = 0.9) +
+        # Error bar for x based on _sigma
+        geom_errorbar(
+          aes(
+            ymin = logSFR_pred - logSFR_pred_sigma,
+            ymax = logSFR_pred + logSFR_pred_sigma
+          ),
+          width = 0.1
+        ) +
+        geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed") +
+        xlim(-8, 1) +
+        labs(
+          title = "Comparison of logSFR_pred and logSFR_total",
+          x = TeX("$log_{10}\\left[\\frac{SFR_{total}}{M_o/yr} \\right]$"),
+          y = TeX("$log_{10}\\left[\\frac{SFR_{pred}}{M_o/yr} \\right]$")
+        )
+
+      ggsave("plots/sfr_comparison.png")
+    }
+  )
 )
