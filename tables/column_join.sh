6bbd2c9e5e310eed04fe6befb6d5eefc4204bf4f
dim-papach <dipapachristopoulos@gmail.com>
2025-02-27 01:20:24 +0200

    added id column

--- tables/column_join.sh
+++ tables/column_join.sh
@@ -12,3 +12,10 @@ stilts tpipe \
   cmd='addcol -units "log(Msol/yr)" -desc "Merge of log SFR" logSFR_total "NULL_logSFR_HEC ? 0.7*logSFR_UNGC-0.6 : logSFR_HEC"'\
   cmd='addcol -units "Msol/yr" -desc "Total SFR in linear scale" SFR_total "exp10(logSFR_total)"'\
   cmd='addcol -units "log(Msol)" -desc "" logM_total "NULL_logM_HEC ? log10(0.82)+merge_logKLum: logM_HEC"'\
+  cmd='addcol -units "Msol" -desc "Total M_* in linear scale" M_total "exp10(logM_total)"'\
+  cmd='addcol -desc "ID column" ID "Index"'\
+
+
+stilts tpipe \
+  in=filled.ecsv \
+  omode=out out=filled.csv\
