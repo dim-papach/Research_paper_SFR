543d44c68586dc74d6467f192e65b111fa2fb617
dim-papach <dipapachristopoulos@gmail.com>
2025-03-09 20:45:21 +0200

    Compare for norm-uni-NR

--- method_comparison/plots.sh
+++ method_comparison/plots.sh
@@ -5,7 +5,7 @@ topcat -stilts plot2plane \
    legend=true legpos=0.0112,0.939 \
    ifmt=CSV x=sSFR size=2 \
    layer_1=Mark \
-      in_1=/home/dp/Documents/Research_paper_SFR/r_mcmc/joined_data.csv \
+      in_1=/home/dp/Documents/Research_paper_SFR/r_mcmc_uni/joined_data.csv \
       y_1=A \
       shading_1=auto color_1=blue \
       leglabel_1=MCMC \
@@ -23,12 +23,15 @@ topcat -stilts plot2plane \
    legend=true legpos=0.9911,0.976 \
    ifmt=CSV x=tau \
    layer_1=Histogram \
-      in_1=/home/dp/Documents/Research_paper_SFR/r_mcmc/joined_data.csv \
+      in_1=/home/dp/Documents/Research_paper_SFR/r_mcmc_uni/joined_data.csv \
       leglabel_1=MCMC color_1=blue\
    layer_2=Histogram \
       in_2=/home/dp/Documents/Research_paper_SFR/NR/filled_with_NR.csv \
       color_2=red \
       leglabel_2=N-R \
+   layer_3=Histogram \
+      in_3=/home/dp/Documents/Research_paper_SFR/r_mcmc_normal/joined_data.csv \
+      leglabel_3="MCMC with \tau\sim normal(4,1)" color_3=green\
    out=/home/dp/Documents/Research_paper_SFR/method_comparison/tau_hist.png
 
 topcat -stilts plot2plane \
@@ -38,7 +41,7 @@ topcat -stilts plot2plane \
    auxmap=viridis auxfunc=histogram auxmin=0.07 auxmax=13.71 \
    auxvisible=true auxlabel='x=t_{sf}/\tau' \
    legend=true legpos=0.0103,0.944 \
-   in=/home/dp/Documents/Research_paper_SFR/r_mcmc/joined_data.csv ifmt=CSV x=logM_total y=A \
+   in=/home/dp/Documents/Research_paper_SFR/r_mcmc_uni/joined_data.csv ifmt=CSV x=logM_total y=A \
     leglabel='log(A_{del}) = 0.86\cdot log(M_*) + 1.5 \\ Correlation=94\%' antialias=true \
    layer_1=Mark \
       aux_1=x \
@@ -48,4 +51,27 @@ topcat -stilts plot2plane \
       weight_3=x \
       color_3=black dash_3=3,3 \
    legseq=_1,_2 \
-   out=/home/dp/Documents/Research_paper_SFR/method_comparison/A_M_MCMC.png
+   out=/home/dp/Documents/Research_paper_SFR/method_comparison/A_M_MCMC_uni.png
+   
+#weighted for x Corr = 98%
+   
+topcat -stilts plot2plane \
+   xpix=1242 ypix=525 \
+   ylog=true xlabel='log(M_*/M_\odot)' ylabel='A_{del}\ [M_\odot]' grid=true texttype=latex \
+   xmin=2.94 xmax=11.4 ymin=1199 ymax=9.713E11 \
+   auxmap=viridis auxfunc=histogram auxmin=0.07 auxmax=13.71 \
+   auxvisible=true auxlabel='x=t_{sf}/\tau' \
+   legend=true legpos=0.0103,0.944 \
+   in=/home/dp/Documents/Research_paper_SFR/r_mcmc_normal/joined_data.csv ifmt=CSV x=logM_total y=A \
+    leglabel='log(A_{del}) = 0.94\cdot log(M_*) + 0.75 \\ Correlation=99\%' antialias=true \
+   layer_1=Mark \
+      aux_1=x \
+      shading_1=aux size_1=2 \
+   layer_2=LinearFit \
+   layer_3=LinearFit \
+      weight_3=x \
+      color_3=black dash_3=3,3 \
+   legseq=_1,_2 \
+   out=/home/dp/Documents/Research_paper_SFR/method_comparison/A_M_MCMC_norm.png
+   
+#weighted for x Corr = 99.5%
